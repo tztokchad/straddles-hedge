be5080f58ee9bde832b476954ad6cae4c9e52c7e
tztokchad <tz@dopex.io>
2022-11-25 16:08:41 +0530

    add syncing, retrieve positions, update abi

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const client = {
 };
 const writerAddress = process.env.WRITER_ADDRESS;
 
-let nextExpiry = "23NOV22";
-
 const bybitSpot = new SpotClientV3();
 const bybitOptions = new USDCOptionClient({
   key: client.key,
@@ -26,29 +24,46 @@ const bybitOptions = new USDCOptionClient({
   testnet: false
 });
 
+// Map closest strikes to positions
+let hedges = {};
+
+/**
+ *
+ * {
+ *  "NOV26-1200-P": {
+ *    writes: 25,
+ *    hedges: 10
+ * }
+ * }
+ */
+
 const toBN = n => web3.utils.toBN(n);
 
 const getLastPrice = async () =>
   parseFloat((await bybitSpot.getLastTradedPrice("ETHUSDT")).result.price);
 
-// Fill puts to hedge new purchases
-const fillPuts = async (lastPrice, toFill) => {
+const getExpirySymbol = (expiry, lastPrice) => {
+  const expiryDate = new Date(expiry * 1000);
+  const month = expiryDate.toLocaleString("default", { month: "short" });
+  const day = expiryDate.getUTCDate();
+  const year = expiryDate
+    .getFullYear()
+    .toString()
+    .slice(-2);
   // Compute strike closest to last price
   let closest = (Math.floor(lastPrice / 25) * 25).toString();
+  return `ETH-${day}${month.toUpperCase()}${year}-${closest}-P`;
+};
 
+// Fill puts to hedge new purchases
+const fillPuts = async (symbol, toFill) => {
   let markets = (await bybitOptions.getContractInfo({
     baseCoin: "ETH",
     limit: 1000
   })).result.dataList;
-  markets = markets.filter(
-    market =>
-      market.symbol.split("-")[1] === nextExpiry &&
-      market.symbol.split("-")[2] === closest &&
-      market.symbol.split("-")[3] === "P"
-  );
+  markets = markets.filter(market => market.symbol === symbol);
 
   // Get orderbooks
-  const symbol = markets[0].symbol;
   let orderbook = (await bybitOptions.getOrderBook(symbol)).result;
 
   // Get best ask
@@ -85,27 +100,67 @@ const fillPuts = async (lastPrice, toFill) => {
   }
 };
 
-// Retrieve bybit portfolio greeks
-const getGreeks = async () => bybitOptions.getAssetInfo("ETH");
-
 // Retrieve bybit portfolio positions
-const getPositions = async () =>
-  bybitOptions.getPositions({ category: "OPTION", baseCoin: "ETH" });
+const getPositions = async expirySymbol => {
+  let positions = await bybitOptions.getPositions({
+    category: "OPTION",
+    baseCoin: "ETH"
+  });
+  return positions.result.dataList.filter(
+    position => position.symbol === expirySymbol
+  );
+};
+
+// Handle previous straddle purchase events for epoch
+const getPreviousPurchases = async currentEpoch =>
+  new Promise(resolve => {
+    ethStraddle.getPastEvents("Purchase", { fromBlock: "" }, (err, events) => {
+      if (!err) {
+        events = events.filter(
+          event => event.returnValues.epoch == currentEpoch
+        );
+        events = events.map(event => event.returnValues);
+        resolve(events);
+      }
+    });
+  });
 
 // Watch straddle purchases
-const watchPurchaseEvents = () => {
+const watchPurchaseEvents = (expiry, poolShare) => {
   ethStraddle.events
     .Purchase({
-      fromBlock: "39885610"
+      fromBlock: "latest"
     })
     .on("connected", () => console.log("Listening for purchase events"))
-    .on("data", event => {
+    .on("data", async event => {
       let { user, straddleId, cost } = event.returnValues;
       cost =
         toBN(cost)
           .div(toBN("100000000000000000000"))
           .toNumber() / 1e6;
-      console.log("New purchase event:", { user, straddleId, cost });
+      let {
+        apStrike,
+        underlyingPurchased
+      } = await ethStraddle.methods.straddlePositions(straddleId).call();
+      apStrike = apStrike / 1e8;
+      underlyingPurchased = underlyingPurchased / 1e18;
+      const premiumPerStraddle = cost / (underlyingPurchased * 2);
+      console.log("New purchase event:", {
+        user,
+        straddleId,
+        cost,
+        premiumPerStraddle
+      });
+      const amountToHedge = (underlyingPurchased * 2 * poolShare) / 100;
+      // Get symbol for Bybit expiry
+      const expirySymbol = getExpirySymbol(expiry, apStrike);
+      console.log(
+        `To hedge: ${amountToHedge} puts sold @ $${apStrike} w/ ${expirySymbol}`
+      );
+      console.log("Checking for active hedges..");
+      let hedgePositions = await getPositions(expirySymbol);
+      console.log({ hedgePositions });
+      await fillPuts(expirySymbol, amountToHedge);
     })
     .on("error", (err, receipt) => {
       console.error("Error on Purchase event:", err, receipt);
@@ -115,16 +170,17 @@ const watchPurchaseEvents = () => {
 (async () => {
   const currentEpoch = await ethStraddle.methods.currentEpoch().call();
   const epochData = await ethStraddle.methods.epochData(currentEpoch).call();
-  const { expiry } = epochData;
+  const { expiry, usdDeposits, underlyingPurchased } = epochData;
   const underlyingPrice = await ethStraddle.methods.getUnderlyingPrice().call();
   const premium =
     toBN(
       await ethStraddle.methods
-        .calculatePremium(true, underlyingPrice, 1e6, expiry)
+        .calculatePremium(true, underlyingPrice, underlyingPrice, 1e6, expiry)
         .call()
     )
       .div(toBN(1e8))
       .toNumber() / 1e6;
+  let lastPrice = await getLastPrice();
   console.log("Straddles data for epoch", currentEpoch, ":", epochData);
 
   // Get all write positions for address
@@ -154,26 +210,29 @@ const watchPurchaseEvents = () => {
       .add(toBN(totalUsdDeposits))
       .toString();
   }
-  let lastPrice = await getLastPrice();
   let availableUsdDeposits = toBN(totalUsdDeposits).toNumber() / 1e6;
   let totalSellableStraddles =
     toBN(totalUsdDeposits).toNumber() / 1e6 / lastPrice;
   let poolShare =
     toBN(totalUsdDeposits)
       .mul(toBN(1e8)) // 1e6 (multiplier) * 1e2 (100%)
-      .div(toBN(epochData.usdDeposits))
+      .div(toBN(usdDeposits))
       .toNumber() / 1e6;
+  let poolPutsToPurchase = (underlyingPurchased * 2) / 1e18;
+  let writerPutsToPurchase = (underlyingPurchased * 2 * poolShare) / 1e20;
   console.log("Total available USD deposits:", availableUsdDeposits);
   console.log("Total sellable straddles:", totalSellableStraddles);
   console.log("Share of pool:", poolShare, "%");
+  console.log("Pool puts to purchase:", poolPutsToPurchase);
+  console.log("Writer puts to purchase:", writerPutsToPurchase);
 
   console.log({ underlyingPrice, premium });
 
-  // watchPurchaseEvents();
-  const greeks = await getGreeks();
-  console.log(greeks.result.dataList);
-
-  const positions = await getPositions();
-  console.log(positions.result.dataList);
-  // await fillPuts(lastPrice, totalSellableStraddles);
+  const previousPurchases = await getPreviousPurchases(
+    currentEpoch,
+    expiry,
+    poolShare
+  );
+  console.log("Previous purchase events:", previousPurchases);
+  watchPurchaseEvents(expiry, poolShare);
 })();
